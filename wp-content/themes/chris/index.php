97422b58ab163ec12ceb46324ae1b29a3146f6e7
Leo Brescia <leonardo@leobrescia.com.br>
2017-08-10 22:06:31 -0300

    home blog

--- wp-content/themes/chris/index.php
+++ wp-content/themes/chris/index.php
@@ -20,7 +20,7 @@ $images = get_field('imagens', 'option');
 <div id="primary" class="content-area" ng-app="app" ng-controller="HomeController as home">
   <main id="main" class="site-main">
 
-    <section class="slider" ng-style="{height:home.height}">
+    <section class="slider" ng-style="{height:home.height}" itemprop="mainEntityOfPage">
         <?php if ($images) : ?>
       <ul class="bxslider">
         <?php foreach ($images as $image) : ?>
@@ -41,6 +41,74 @@ $images = get_field('imagens', 'option');
         <?php endif; ?>
     </section>
     <!-- slider -->
+
+    <?php 
+      // WP_Query arguments
+      $args = array(
+        'posts_per_page' => '3',
+      );
+
+      // The Query
+      $query = new WP_Query( $args );
+
+      if ( $query->have_posts() ):
+      ?>
+
+    <section class="container blog__home">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="title center-block">
+            <h1>BLOG</h1>
+            <span>Veja um pouco das histórias <br>que já tive o prazer de contar</span>
+          </div>
+        </div>
+        <!-- col-xs-12 -->
+
+        <?php while ( $query->have_posts() ): $query->the_post(); ?>
+          
+        <article  class="col-sm-4" itemid="<?php the_ID(); ?>" id="post-<?php the_ID(); ?>" <?php post_class(); ?>  itemscope itemtype=" http://schema.org/BlogPosting">
+          <span itemprop="author " itemscope itemtype="http://schema.org/Person" style="display:none">
+            <span itemprop="name">Chris Bonfatti</span>
+          </span>
+          <time datetime="<?= get_the_date('c'); ?>" itemprop="datePublished" style="display:none"></time>
+			    <time datetime="<?php the_modified_date('c'); ?>" itemprop="dateModified"  style="display:none"></time>
+          <figure itemprop="image" itemscope itemtype="http://schema.org/ImageObject" style="display:none">
+            <meta itemprop="url" content="<?= get_the_post_thumbnail_url(); ?>" />
+						<?php 
+							$image = wp_get_attachment_image_src( get_post_thumbnail_id(),"full");
+						?>
+						<meta itemprop="height" content="<?= $image[2]; ?>" />
+						<meta itemprop="width" content="<?= $image[1]; ?>" />
+						<?php echo get_the_post_thumbnail( get_the_ID(), 'full', array( 'itemscope' => 'image' ) );  ?>
+          </figure>
+
+          <div class="image__wapper">
+            <div class="image__content" style=" background-image: url('<?= $image[0]; ?>')">
+              <h2 class="entry-title" itemprop="name headline">
+                <a itemprop="url" href="<?= esc_url( get_permalink() ); ?>" rel="bookmark" title="<?= get_the_title(); ?>">
+                  <?php the_title(); ?>
+                  <?php $categories = get_the_category(); ?> <br>
+                  <span class="sub__titulo"> 
+                    <span itemprop="genre"><?= $categories[0]->cat_name; ?></span>
+                    <?php if(get_field('local')): ?>
+                      || <?= get_field('local'); ?>
+                    <?php endif; ?>
+                  </span>
+                </a>
+              </h2>
+            </div>
+          </div>
+        </article >
+        <?php endwhile; ?>
+      </div>
+      <!-- row -->
+    </section>
+    <!-- blog__home -->
+    <?php
+    endif;
+    // Restore original Post Data
+    wp_reset_postdata();
+    ?>
   </main><!-- #main -->
 </div><!-- #primary -->
 
